8e68e02abfa4b74a08807e09736fb0eac637d8f0
Pavel Moravec <pavelm@salsitasoft.com>
2022-10-21 10:55:45 +0200

    add logos

--- src/App.tsx
+++ src/App.tsx
@@ -10,14 +10,17 @@ function App() {
   return (
     <div className="App">
       <div>
+        <a href="https://vanilla-extract.style/" target="_blank">
+          <img src="/vanilla-extract-alpha.png" className="logo" alt="vanilla-extract logo" />
+        </a>
         <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
+          <img src="/vite-alpha.png" className="logo" alt="Vite logo" />
         </a>
         <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src="/react-alpha.png" className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1><a href="https://www.salsitasoft.com" target="_blank">Salsita</a> ❤️ <a href="https://vanilla-extract.style/" target="_blank">Vanilla-Extract</a> + <a href="https://vitejs.dev" target="_blank">Vite</a> + <a href="https://reactjs.org" target="_blank">React</a></h1>
       <Card title="Hello from a Card">
         <Button onClick={() => setCount((count) => count + 1)} variant={ count % 2 === 0 ? 'primary' : 'secondary'}>
           count is {count}
